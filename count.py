d7a1aa48dfb0b9081430c0d8e4b4079778a5847d
monteirocaio91 <garcia.monteiro.caio@gmail.com>
2025-03-28 08:40:58 -0300

    Add files via upload

---
+++ count.py
@@ -0,0 +1,9 @@
+minha_lista = [1, 2, 2, 3, 4, 2, 5, 6, 2]
+
+# Elemento que queremos contar
+elemento = 2
+
+# Usando o método count() para contar as ocorrências
+quantidade = minha_lista.count(elemento)
+
+print(f"A quantidade de vezes que {elemento} aparece na lista é: {quantidade}")
